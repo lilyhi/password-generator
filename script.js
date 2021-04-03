6cce7b9654eefff9e598884b290dfcb91db7d220
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-04-03 12:57:49 -0700

    added variables and some functions

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 // Assignment code here
-// test again
+var confirmLength = "";
+var confirmLowerCase = "abcdefghijklmnopqrstuvwxyz";
+var confirmUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var confirmNumeric = "0123456789";
+var confirmSpecialCharacters = " !@#$%^&*()_+~`|}{[]\:;?><,./-=";
+
+
+// generate password function 
+var generatePassword = function() {
+  var confirmLength = prompt("How many characters would you like your password to be?");
+  
+  while (confirmLength <= 7 || confirmLength >= 128) {
+    window.alert("Password must be between 8-128 characters!");
+    var confirmLength = prompt("How many characters would you like your password to be?");
+
+
+    var confirmLowerCase = window.confirm("Please click Ok to confirm use of lower case characters");
+    var confirmUpperCase = window.confirm("Pleaes click Ok to confirm use of upper case characters");
+    var confirmNumeric = window.confirm("Please click Ok to confirm use of numbers");
+    var confirmSpecialCharacters = window.confirm("Please click Ok to confirm use of special characters");
+
+    // denied all confirms 
+    while (confirmLowerCase === false && confirmUpperCase === false && confirmNumeric === false && confirmSpecialCharacters === false);
+      window.alert("Please choose one of the following options to create password!");
+      var confirmLowerCase = window.confirm("Please click Ok to confirm use of lower case characters");
+      var confirmUpperCase = window.confirm("Pleaes click Ok to confirm use of upper case characters");
+      var confirmNumeric = window.confirm("Please click Ok to confirm use of numbers");
+      var confirmSpecialCharacters = window.confirm("Please click Ok to confirm use of special characters");
+
+  }
+}
+
+
+
 
 
 // Get references to the #generate element
